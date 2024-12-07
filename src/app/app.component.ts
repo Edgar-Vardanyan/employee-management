874b8483d0eef81a3f0a6bebf7e1ea19a673ecca
Edgar-Vardanyan <edgarvardanians@gmail.com>
2024-12-07 16:10:45 +0400

    Added Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
